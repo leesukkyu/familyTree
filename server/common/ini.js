d24f1a22ac55757873f20cbf0d0d7ec1db591387
leesukkyu <ansua86@naver.com>
2019-07-17 20:40:06 +0900

    Delete ini.js
    Delete ini.js

--- server/common/ini.js
+++
@@ -1,8 +0,0 @@
-var INI = {
-    DB_USER: 'ansua86',
-    DB_PASSWORD: 'tjrrnddl1!'
-}
-
-INI.DB_URL = 'mongodb+srv://' + INI.DB_USER + ':' + INI.DB_PASSWORD + '@sdm-pmbuu.mongodb.net/test?retryWrites=true'
-
-module.exports = INI;
